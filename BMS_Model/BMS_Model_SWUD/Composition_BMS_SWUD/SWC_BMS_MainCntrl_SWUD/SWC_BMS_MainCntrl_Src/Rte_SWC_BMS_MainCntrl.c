a93170f977778e502ff0e27640128c30113f80c5
MDS-Seonmin-Kang <seonmin.kang@mdsit.co.kr>
2026-01-29 16:12:20 +0900

    RE SoX_1000ms 내부 수정
    1. 변수 추가 (이름 수정 예정)
    2. Rte Read를 통해 값 획득
    3. Soc에 사용 될 값 설정
    4. Soc Process 진행
    5. 결과 Write 진행

--- BMS_Model/BMS_Model_SWUD/Composition_BMS_SWUD/SWC_BMS_MainCntrl_SWUD/SWC_BMS_MainCntrl_Src/Rte_SWC_BMS_MainCntrl.c
+++ BMS_Model/BMS_Model_SWUD/Composition_BMS_SWUD/SWC_BMS_MainCntrl_SWUD/SWC_BMS_MainCntrl_Src/Rte_SWC_BMS_MainCntrl.c
@@ -1,6 +1,7 @@
 #include "Rte_SWC_BMS_MainCntrl.h"
 #include "BMS_MainCntrl_GlobalData.h"
 #include "Bms_StatusDetermine.h"
+#include "BmsMainCntrl_Soc.h"
 
 /* ==================================================================================
  * Global Variables Definition (실제 메모리 할당)
@@ -11,6 +12,17 @@ Input_CellMeas_Type  g_Input_CellMeas;
 Input_CurrMeas_Type  g_Input_CurrMeas;
 Input_TempMeas_Type  g_Input_TempMeas;
 Input_FaultFlag_Type g_Input_FaultFlag;
+
+/* Soc 판단용 선언 */
+Std_ReturnType ret;
+uint32  packVoltageSum_u32;
+sint32  packCurrent_s32;
+sint16  cellTempAvg_s16;
+e_IgnStat ignSignal;
+boolean chgConnectedFlag;
+
+BmsSoc_Input_t in;
+BmsSoc_Output_t out;
 
 /* 내부 상태 관리 변수 */
 static e_VcuCanCmd g_BmsCurrentMode = Standby;
@@ -24,6 +36,44 @@ FUNC(void, SWC_BMS_MainCntrl_CODE) REtSWC_BMS_MainCntrl_BattStatusProcess_10ms()
 FUNC(void, SWC_BMS_MainCntrl_CODE) REtSWC_BMS_MainCntrl_SoX_1000ms(void)
 {
     //Workspace by KSM.
+    /* Read Section */
+    /* 1) RTE Read */
+    ret = Rte_Read_R_CellMeasData_packVoltageSum(&packVoltageSum_u32);
+    (void)ret;
+
+    ret = Rte_Read_R_CurrMeasData_packCurrent(&packCurrent_s32);
+    (void)ret;
+
+    ret = Rte_Read_R_TempMeasData_cellTempAverage(&cellTempAvg_s16);
+    (void)ret;
+
+    ret = Rte_Read_R_SignalInput_ignSignal(&ignSignal);
+    (void)ret;
+
+    ret = Rte_Read_R_SignalInput_chargeConnectedFlag(&chgConnectedFlag);
+    (void)ret;
+
+    /* 2) SoC 입력 구성 (현재 SoC 모듈 타입에 맞춰 축소 캐스팅) */
+    in.packVoltage_mV  = Bms_ClampU32ToU16(packVoltageSum_u32); /* TODO: SoC 모듈이 uint32로 받는 게 더 정확 */
+    in.packCurrent_mA  = Bms_ClampS32ToS16(packCurrent_s32);    /* TODO: SoC 모듈이 sint32로 받는 게 더 정확 */
+    in.packTemp_dC     = cellTempAvg_s16;
+
+    /* ignOn/chgConnected는 “존재 여부”만 필요해서 0/1로 정규화 */
+    in.ignOn           = (ignSignal != (e_IgnStat)0) ? (uint8)1u : (uint8)0u;
+    in.chgConnected    = (chgConnectedFlag != (boolean)0) ? (uint8)1u : (uint8)0u;
+
+    /* 3) SoC Process 진행 */
+    BmsMainCntrl_Soc_Process_1000ms(&in, &out);
+    
+     /* 4) RTE Write (PackCalculatedData_Tx) */
+    (void)Rte_Write_P_PackCalculatedData_Tx_socLevel(out.soc_x10);
+
+    /* RTE는 sohLevel이 uint32라서 확장 캐스팅 */
+    (void)Rte_Write_P_PackCalculatedData_Tx_sohLevel((uint32)out.soh_x10);
+
+    /*  HMI 게이지: 0~100%로 보고 싶으면 out.soc_x10(0~1000)을 0~100으로 변환 */
+    (void)Rte_Write_P_HMI_SigOutput_socGauge((uint8)(out.soc_x10 / 10u));
+
 }
 
 FUNC(void, SWC_BMS_MainCntrl_CODE) REtSWC_BMS_MainCntrl_PerfomCellBalancing_1000ms(void)
